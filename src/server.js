2448298615657abfee6479e0ac68f4df19b2f832
Rafael Malaga <rafaelmalagach@gmail.com>
2025-09-28 20:28:49 -0600

    Fix: usar process.env.PORT para Render

--- src/server.js
+++ src/server.js
@@ -6,29 +6,25 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // permite conexiones desde cualquier origen (web/app)
+    origin: "*",
   }
 });
 
-// Cuando un cliente se conecta
 io.on('connection', (socket) => {
-  console.log('Cliente conectado ✅', socket.id);
+  console.log('Cliente conectado ', socket.id);
 
-  // Escuchar mensajes simples
   socket.on('mensaje', (data) => {
     console.log('Mensaje recibido:', data);
-
-    // Reenviar a todos los conectados
     io.emit('mensaje', data);
   });
 
-  // Al desconectarse
   socket.on('disconnect', () => {
-    console.log('Cliente desconectado ❌', socket.id);
+    console.log('Cliente desconectado ', socket.id);
   });
 });
 
-// Levantar el servidor
-server.listen(3000, () => {
-  console.log('Servidor WebSocket corriendo en http://localhost:3000');
+// Usar el puerto asignado por Render
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Servidor WebSocket corriendo en http://localhost:${port}`);
 });
